3cfdd9f612c8b4d01d8634f5feebe5d3ad88cb83
杨码云 <18832026962@163.com>
2021-03-22 15:06:54 +0800

    one

--- src/Number_of_1_Bits.java
+++ src/Number_of_1_Bits.java
@@ -3,6 +3,8 @@ public class Number_of_1_Bits {
     public static void main(String[] args) {
 
         System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
 
     }
 }
