4b44d896a05a6a379cb6a4b78045c14d3d2742b2
杨码云 <18832026962@163.com>
2021-03-24 14:30:43 +0800

    one

---
+++ src/_132_Pattern.java
@@ -0,0 +1,45 @@
+import java.util.Deque;
+import java.util.Stack;
+
+public class _132_Pattern {
+
+    public static boolean find132pattern(int[] nums) {
+        int[] minleft=new int[nums.length];
+        //int[] maxright=new int[nums.length];
+        Stack<Integer> stack=new Stack();
+        int second=nums[nums.length-1];
+
+        minleft[0]=nums[0];
+        for (int i = 1; i < nums.length-1; i++) {
+            minleft[i]=Math.min(nums[i-1],minleft[i-1]);
+        }
+
+        for (int i = nums.length-1; i >0; i--) {
+            if (stack.empty()){
+                stack.push(nums[i]);
+            }else {
+                while (!stack.empty()&&stack.peek()<nums[i]){
+                   second=stack.pop();
+                }
+                stack.push(nums[i]);
+
+                if (second>minleft[i]&&second<nums[i]){
+                    return true;
+                }
+            }
+
+        }
+        return false;
+
+
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(find132pattern(new int[]{3,1,4,2}));
+    }
+
+
+
+}
