7acf6bc9c8b75affbdcaeaec4c1de0db984427bc
杨码云 <18832026962@163.com>
2021-03-25 16:37:41 +0800

    one

---
+++ src/Remove_Duplicates_from_Sorted_List_II.java
@@ -0,0 +1,39 @@
+public class Remove_Duplicates_from_Sorted_List_II {
+
+    public ListNode deleteDuplicates(ListNode head) {
+
+        if (head==null||head.next==null){
+            return head;
+        }
+
+        ListNode newhead=new ListNode(101,head);
+
+        ListNode start=newhead;
+        ListNode font=head;
+        ListNode back=head.next;
+        while (font!=null&&back!=null){
+            if (font.val!=back.val){
+                start.next=font;
+                start=start.next;
+                font=font.next;
+                back=back.next;
+            }else {
+                while (back!=null&&font.val==back.val){
+                    back=back.next;
+                }
+                if (back==null){
+                    start.next=back;
+                    return newhead.next;
+                }else {
+                    font=back;
+                    back=back.next;
+                }
+            }
+        }
+        start.next=font;
+
+        return newhead.next;
+
+
+    }
+}
