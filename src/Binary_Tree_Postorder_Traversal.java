f6d9aaee68c50ffcde83f8ceabbc39bc260e3e0c
isPoikilotherm <18832026962@163.com>
2021-04-11 19:24:40 +0800

    Binary_Tree_Postorder_Traversal

---
+++ src/Binary_Tree_Postorder_Traversal.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Binary_Tree_Postorder_Traversal {
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans=new ArrayList<>();
+        if (root==null){ return ans; }
+
+        Stack<TreeNode> stack=new Stack<>();
+        TreeNode pre=null;
+        while (root!=null||!stack.empty()){
+            while (root!=null){
+                stack.add(root);
+                pre=root;
+                root=root.left;
+            }
+            root=stack.pop();
+            if (root.right==null||pre==root.right){
+                ans.add(root.val);
+                pre=root;
+                root=null;
+            }else {
+                stack.add(root);
+                root=root.right;
+                pre=root;
+            }
+
+        }
+        return ans;
+
+    }
+}
