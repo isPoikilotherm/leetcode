36eec2dc13da6169e9de625a417ea9f7128485a4
isPoikilotherm <18832026962@163.com>
2021-04-28 13:50:23 +0800

    Sum_of_Square_Numbers

---
+++ src/Sum_of_Square_Numbers.java
@@ -0,0 +1,24 @@
+public class Sum_of_Square_Numbers {
+
+
+    public boolean judgeSquareSum(int c) {
+        double d_c=(double)c;
+        double n=Math.sqrt(d_c);
+        int count=(int)n;
+
+        for (int i = count,j=0; i >=j ; ) {
+            if(i*i+j*j==c){
+                return true;
+            }
+            if(i*i+j*j<c){
+                j++;
+            }else if(i*i+j*j>c){
+                i--;
+            }
+
+        }
+        return false;
+    }
+
+
+}
