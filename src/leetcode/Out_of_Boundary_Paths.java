df0590286796853ccb66fff7b583b26f8121afb5
isPoikilotherm <18832026962@163.com>
2021-08-15 18:36:11 +0800

    Out_of_Boundary_Paths

---
+++ src/leetcode/Out_of_Boundary_Paths.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Out_of_Boundary_Paths {
+    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        final int MOD = 1000000007;
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int outCounts = 0;
+        int[][][] dp = new int[maxMove + 1][m][n];
+        dp[0][startRow][startColumn] = 1;
+        for (int i = 0; i < maxMove; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < n; k++) {
+                    int count = dp[i][j][k];
+                    if (count > 0) {
+                        for (int[] direction : directions) {
+                            int j1 = j + direction[0], k1 = k + direction[1];
+                            if (j1 >= 0 && j1 < m && k1 >= 0 && k1 < n) {
+                                dp[i + 1][j1][k1] = (dp[i + 1][j1][k1] + count) % MOD;
+                            } else {
+                                outCounts = (outCounts + count) % MOD;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return outCounts;
+
+    }
+}
